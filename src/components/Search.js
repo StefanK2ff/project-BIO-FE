34373f840ed0a83279434a028fbda63a5cf2245c
Stefan Berk <stefan@k2ff.de#>
2020-03-04 15:32:05 +0100

    basic search on google book api implemented

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,30 +1,61 @@
-import React, { Component } from 'react'
-import {fullSearch} from "./../lib/bookAPI-helper"
+import React, { Component } from "react";
+import fullSearch from "./../lib/bookAPI-helper";
 
 export default class Search extends Component {
-    state = {
-        query: ""
-    }
+  state = {
+    query: "",
+    results: []
+  };
 
-    formHandleChange = e => {
-        e.preventDefault();
-        let { value, name  } = e.target;
-        this.setState({ [name]: value });
-        fullSearch(value)
-    };
-
-    render() {
-        return (
-            <div>
-                <form>
-                <input
-                    type="text"
-                    name="query"
-                    value={this.state.query}
-                    onChange={this.formHandleChange}
-                />
-                </form>
-            </div>
-        )
+  formHandleChange = async e => {
+    e.preventDefault();
+    let { value, name } = e.target;
+    this.setState({ [name]: value });
+    try {
+      let results = await fullSearch(value);
+      this.setState({ results: results });
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Search</h2>
+        <form>
+          <input
+            type="text"
+            name="query"
+            value={this.state.query}
+            onChange={this.formHandleChange}
+          />
+        <input type="radio" id="all" name="searchFilter" value="" />
+          <label for="huey">All Field</label>
+
+          <input type="radio" id="intitle:" name="searchFilter" value="intitle:" />
+          <label for="huey">Just Title</label>
+
+          <input type="radio" id="inauthor:" name="searchFilter" value="inauthor:" />
+          <label for="dewey">Just Author</label>
+
+          <input type="radio" id="inpublisher:" name="searchFilter" value="inpublisher:" />
+          <label for="louie">Just Publisher</label>
+
+          <input type="radio" id="isbn:" name="searchFilter" value="isbn:" />
+          <label for="louie">Just ISBN</label>
+        </form>
+        <h2>Result</h2>
+        <ul>
+          {this.state.results.map((book, index) => {
+            return (
+              <li>
+                {index} - {book.volumeInfo.title}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
