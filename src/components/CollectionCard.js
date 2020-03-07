3cc74c7aab92655a4c6360d7cc8f9dae38bef7eb
Stefan Berk <stefan@k2ff.de#>
2020-03-07 11:23:47 +0100

    toggle between edit mode and normal mode implemented

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -4,29 +4,51 @@ import { addBookToCollection } from "./../lib/collections-services";
 import { withAuth } from "./../lib/Auth";
 
 export default class CollectionCard extends Component {
-    state = {
-        newName: "",
-        editmode: false
-    }
-    nameEditField = (e) => {
-        e.preventDefault();
-        this.setState({editmode: true})
-    }
-    
-    formHandleChange = e => {
-        e.preventDefault();
-        let { value, name } = e.target;
-        this.setState({ [name]: value });
-    }
+  state = {
+    newName: "",
+    editmode: false
+  };
+  nameEditField = e => {
+    e.preventDefault();
+    this.setState({ editmode: true, newName:this.props.collection.name }); // anti pattern - ok?
+  };
 
-    render() {
-        const {name, items} = this.props.collection
-        return (
-            <div>
-                {this.state.editmode
-                ? (<li><button onClick={this.saveName}>Save name</button> {name}, with {items.length} item(s) </li>)
-                : (<li><button onClick={this.nameEditField}>Edit name</button> {name}, with {items.length} item(s) </li>)}
-            </div>
-        )
-    }
+  formHandleChange = e => {
+    e.preventDefault();
+    let { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  saveName = async e => {
+    e.preventDefault();
+
+    this.setState({ editmode: false, newName:"" })
+  }
+
+  render() {
+    const { name, items } = this.props.collection;
+    return (
+      <div>
+        {this.state.editmode ? (
+          <li>
+            <form className="inlineEdit">
+              <button onClick={this.saveName}>Save name</button>{" "}
+              <input
+                type="text"
+                name="newName"
+                value={this.state.newName}
+                onChange={this.formHandleChange}
+              />
+            </form>
+            , with {items.length} item(s){" "}
+          </li>
+        ) : (
+          <li>
+            <button onClick={this.nameEditField}>Edit name</button> {name}, with{" "}
+            {items.length} item(s){" "}
+          </li>
+        )}
+      </div>
+    );
+  }
 }
