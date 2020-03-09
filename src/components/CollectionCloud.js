f9dfdb33b31743ec61fb28e35b49cbe13397a145
Stefan Berk <stefan@k2ff.de#>
2020-03-09 11:55:24 +0100

    working version of collectin cloud implemented in library listing

--- src/components/CollectionCloud.js
+++ src/components/CollectionCloud.js
@@ -1,74 +1,122 @@
-import React from 'react'
-import { withAuth } from '../lib/Auth'
+import React, { Component } from "react";
+import { withAuth } from "../lib/Auth";
+import {
+  createCollectionWithItems,
+  addBookToCollection
+} from "./../lib/collections-services";
 
 /* NOTES 
     This modulre requires following Data
     - collections from the user (Props)
     - A Book ID
-    - Needs nothing to store in a state (stateless)
+    - Needs a minimal state (just for the form)
     Functions
     1) removeFromCollection
     2) addToCollection
     3) addToNewColl
 */
 
-function CollectionCloud() {
+class CollectionCloud extends Component {
+  state = {
+    newCollection: ""
+  };
+
+  addToCollection = e => {
+    e.preventDefault();
+    let itemList = this.getItemList(e.target.id);
+    let newItemList = [];
+    newItemList.push(
+      e.target.attributes.getNamedItem("bookid").value,
+      ...itemList
+    );
+    addBookToCollection(e.target.id, newItemList, e.target.name); // collectionId, items, name
+    this.props.refresh(this.props.user._id);
+    this.forceUpdate();
+  };
+
+  removeFromCollection = e => {
+    e.preventDefault();
+    let itemList = this.getItemList(e.target.id);
+    const i = itemList.indexOf(
+      e.target.attributes.getNamedItem("bookid").value
+    );
+    if (i > -1) itemList.splice(i, 1);
+    addBookToCollection(e.target.id, itemList, e.target.name); // collectionId, items, name
+    this.props.refresh(this.props.user._id);
+    this.forceUpdate();
+  };
+
+  getItemList = collectionID => {
+    return this.props.user.collections.filter(oneCol => {
+      return oneCol._id === collectionID;
+    })[0].items;
+  };
+
+  formHandleChange = async e => {
+    e.preventDefault();
+    let { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  addToNewColl = e => {
+    e.preventDefault();
+    createCollectionWithItems(
+      this.props.user._id,
+      [e.target.attributes.getNamedItem("bookid").value],
+      this.state.newCollection
+    ); //owner, items, name
+    this.setState({newCollection: ""}, () => this.props.refresh(this.props.user._id));
+  };
+
+  render() {
     return (
-        <div>
-            <div>
-                {this.props.user.collections.map(collection => {
-                  if (collection.items.includes(book.id)) {
-                    return (
-                      <li>
-                        <button
-                          onClick={this.removeFromCollection}
-                          id={collection._id}
-                          bookid={book.id}
-                          name={collection.name}
-                          variant="dark"
-                        >
-                          Remove from {collection.name}
-                        </button>
-                      </li>
-                    );
-                  } else {
-                    return (
-                      <li>
-                        <button
-                          onClick={this.addToCollection}
-                          id={collection._id}
-                          bookid={book.id}
-                          name={collection.name}
-                          variant="light"
-                        >
-                          Add to {collection.name}
-                        </button>
-                      </li>
-                    );
-                  }
-                })}
-                {/* <li>
-                  Create new collection:
-                  <form>
-                    <input
-                      type="text"
-                      name="newCollection"
-                    //   value={this.state.newCollection}
-                      onChange={this.formHandleChange}
-                    />
-                    <button
-                      type="submit"
-                      bookid={this.state.book.id}
-                    //   onClick={this.addToNewColl}
-                    >
-                      Create + add
-                    </button>
-                  </form>
-                </li> */}
-            </div>
-        </div>
-    )
+      <div>
+        {this.props.user.collections.map(collection => {
+          if (collection.items.includes(this.props.bookId)) {
+            return (
+              <button
+                key={collection._id}
+                onClick={this.removeFromCollection}
+                id={collection._id}
+                bookid={this.props.bookId}
+                name={collection.name}
+                variant="dark"
+              >
+                Remove from {collection.name}
+              </button>
+            );
+          } else {
+            return (
+              <button
+                onClick={this.addToCollection}
+                id={collection._id}
+                bookid={this.props.bookId}
+                name={collection.name}
+                variant="light"
+              >
+                Add to {collection.name}
+              </button>
+            );
+          }
+        })}
+        <form>
+          <input
+            type="text"
+            name="newCollection"
+            placeholder="#newCollection"
+            onChange={this.formHandleChange}
+          />
+          <button
+            type="submit"
+            bookid={this.props.bookId}
+            onClick={this.addToNewColl}
+          >
+            Create + add
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-
-export default withAuth(CollectionCloud)
+export default withAuth(CollectionCloud);
---
+++ src/lib/collections-helpers.js
@@ -0,0 +1,9 @@
+import {
+    createCollectionWithItems,
+    addBookToCollection
+  } from "./../lib/collections-services";
+  import { withAuth } from "./../lib/Auth";
+
+
+
+export {addToCollection, removeFromCollection};
