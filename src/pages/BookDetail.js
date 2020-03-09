3d20640958c6abb178e8bfb20ae70203f07e0194
Stefan Berk <stefan@k2ff.de#>
2020-03-09 12:01:50 +0100

    implemented refactored collection cloud successfully in book detail

--- src/pages/BookDetail.js
+++ src/pages/BookDetail.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { withAuth } from "./../lib/Auth";
 import { getBook } from "./../lib/bookAPI-helper";
 import {createCollectionWithItems, addBookToCollection} from "./../lib/collections-services";
+import CollectionCloud from "../components/CollectionCloud";
 
 class BookDetail extends Component {
   constructor(props) {
@@ -9,43 +10,7 @@ class BookDetail extends Component {
     this.state = {
       book: {},
       loading: true,
-      newCollection: ""
     };
-  }
-
-  formHandleChange = async e => {
-    e.preventDefault();
-    let { value, name } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  addToNewColl = e => {
-    e.preventDefault();
-    createCollectionWithItems(this.props.user._id, [this.state.book.id], this.state.newCollection) //owner, items, name
-    this.props.refresh(this.props.user._id);
-    this.setState({newCollection: ""})
-  }
-
-  getItemList = (collectionID) => {
-    return this.props.user.collections.filter(oneCol => {return oneCol._id === collectionID})[0].items
-  }
-
-  addToCollection = (e) => {
-    e.preventDefault();
-    let itemList = this.getItemList(e.target.id)
-    let newItemList = []
-    newItemList.push(this.state.book.id, ...itemList);
-    addBookToCollection(e.target.id, newItemList, e.target.name) // collectionId, items, name
-    this.props.refresh(this.props.user._id);
-  }
-
-  removeFromCollection = (e) => {
-    e.preventDefault();
-    let itemList = this.getItemList(e.target.id)
-    const i = itemList.indexOf(this.state.book.id);
-    if (i > -1) itemList.splice(i, 1);
-    addBookToCollection(e.target.id, itemList, e.target.name) // collectionId, items, name
-    this.props.refresh(this.props.user._id);
   }
 
   componentDidMount() {
@@ -53,12 +18,6 @@ class BookDetail extends Component {
     const { id } = this.props.match.params;
     let bookProm = getBook(id);
     bookProm.then(result => this.setState({ book: result, loading: false }));
-  }
-
-  inThisColl = (collectionID) => {
-    let collectionItems = this.props.user.collections.filter(oneCol => {return oneCol._id === collectionID})[0].items
-    if (collectionItems.includes(this.state.book.id)) return true;
-    else return false
   }
 
   render() {
@@ -75,35 +34,13 @@ class BookDetail extends Component {
         <div>
           {/* <img src={imageLinks.small} alt="" /> */}
           <h1>Detail of {title}</h1>
-          <h2>From {authors.map(author => author)}</h2>
+          <h2>From {authors.map(author => author+" ")}</h2>
           <p>      
             Published {publishedDate} by {publisher}
           </p>
           <p dangerouslySetInnerHTML={{__html: description}} />
 
-
-          <h3>Add to your collections</h3>
-          <ul>
-          {this.props.user.collections.map((collection) => {
-            if (this.inThisColl(collection._id)) {
-              return <li> <button onClick={this.removeFromCollection} id={collection._id} name={collection.name} variant="dark"> Remove from {collection.name}</button></li>
-            } else {
-              return <li> <button onClick={this.addToCollection} id={collection._id} name={collection.name} variant="light"> Add to {collection.name}</button></li>
-            }
-            
-          })}
-          <li>Create new collection: 
-          <form>
-            <input
-            type="text"
-            name="newCollection"
-            value={this.state.newCollection}
-            onChange={this.formHandleChange}
-          /><button type="submit" bookid={this.state.book.id} onClick={this.addToNewColl}>Create + add</button>
-
-          </form>     
-          </li>
-          </ul>
+          <CollectionCloud bookId={this.state.book.id} />
         </div>
 
       );
