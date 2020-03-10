97daa965b6f7ab32b841f212817978cc2c82cb59
Stefan Berk <stefan@k2ff.de#>
2020-03-10 11:44:34 +0100

    implemented hidden navigation menue

--- src/App.js
+++ src/App.js
@@ -38,12 +38,12 @@ class App extends Component {
           <PrivateRoute exact path="/library" component={Library} />
           <PrivateRoute exact path="/book/:id" component={BookDetail} />
         </Switch>
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossorigin="anonymous"
-        />
+        /> */}
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,111 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { withAuth } from "./../lib/Auth";
+import Typography from "@material-ui/core/Typography";
 
-class Navbar extends Component {
-  render() {
-    const { user, logout, isLoggedIn } = this.props;
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 
-    return (
-      <nav className="navbar">
-        <Link to={"/"} id="home-btn">
-          <h4>Home</h4>
-        </Link>
-        
-        {isLoggedIn ? (
-          <>
-            <p>email: {user.email}</p>
-            <Link to={"/collections"} id="home-btn"><h4>My Collections</h4></Link>
-            <Link to={"/library"} id="home-btn"><h4>My Library</h4></Link>
-            <button onClick={logout}>Logout</button>
-          </>
-        ) : (
-          <>
-            <Link to="/login">
-              {" "}
-              <button className="navbar-button">Login</button>{" "}
-            </Link>
-            <br />
-            <Link to="/signup">
-              {" "}
-              <button className="navbar-button">Sign Up</button>{" "}
-            </Link>
-          </>
-        )}
-      </nav>
-    );
+const useStyles = makeStyles({
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
   }
+});
+
+function Navbar(props) {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
+
+  const toggleDrawer = (side, open) => event => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      {props.isLoggedIn ? (
+        <List>
+          <Typography className={classes.root}>
+            <ListItem
+              button
+              key={"My Collections"}
+              component={Link}
+              to="/collections"
+            >
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+              <ListItemText primary={"My Collections"} />
+            </ListItem>
+
+            <ListItem button key={"My Library"} component={Link} to="/library">
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+              <ListItemText primary={"My Library"} />
+            </ListItem>
+
+            <Divider />
+
+            <ListItem button key={"Logout"} onClick={props.logout}>
+              <ListItemIcon><InboxIcon /></ListItemIcon>
+              <ListItemText primary={"Logout"} />
+            </ListItem>
+          </Typography>
+        </List>
+      ) : (
+        <List>
+          <ListItem button key={"Login"} component={Link} to="/login">
+            <ListItemIcon><InboxIcon /></ListItemIcon>
+            <ListItemText primary={"Login"} />
+          </ListItem>
+
+          <ListItem button key={"Sign Up"} component={Link} to="/signup">
+            <ListItemIcon><InboxIcon /></ListItemIcon>
+            <ListItemText primary={"Sign Up"} />
+          </ListItem>
+        </List>
+      )}
+    </div>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer("right", true)}>Menue</Button>
+      <SwipeableDrawer
+        anchor="right"
+        open={state.right}
+        onClose={toggleDrawer("right", false)}
+        onOpen={toggleDrawer("right", true)}
+      >
+        {sideList("right")}
+      </SwipeableDrawer>
+    </div>
+  );
 }
 
 export default withAuth(Navbar);
